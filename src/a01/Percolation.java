69816dea7f5ef61c42a60c31bf7970cc0c35d82d
blake <teakowg@gmail.com>
2017-09-21 14:06:42 -0600

    Outlines required methods in Percolation class

--- src/a01/Percolation.java
+++ src/a01/Percolation.java
@@ -1,5 +1,33 @@
 package a01;
 
 public class Percolation {
+	
+	public Percolation(int n) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void open(int row, int col) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isFull(int row, int col) {
+		return false;
+	}
+
+	public boolean isOpen(int row, int col) {
+		return false;
+	}
+
+	public String numberOfOpenSites() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean percolates() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
+
